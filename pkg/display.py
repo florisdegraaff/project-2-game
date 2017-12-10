7b35dfcd54a224af35041fb3fc8ec79be95b508a
Floris de Graaff <florisdegraaff@hotmail.com>
2017-12-10 19:29:25 +0100

    Added set_title function to the display. Added level numbering in the display title.

--- pkg/display.py
+++ pkg/display.py
@@ -3,6 +3,7 @@ import pkg.essentials as essentials
 clock = essentials.pygame.time.Clock()
 window_size = (1280, 720)
 background_color = (122, 122, 122)
+screenTitle = ""
 
 def prepare_screen():
     global window_size
@@ -13,11 +14,11 @@ def prepare_screen():
 
 def update():
     global clock
-    
+
     essentials.pygame.display.flip()
     clock.tick(30)
     fps = round(clock.get_fps(), 1)
-    essentials.pygame.display.set_caption("fps: " + str(fps))
+    essentials.pygame.display.set_caption("fps: " + str(fps) + " - " + screenTitle)
 
 def set_background(red, green, blue):
     global background_color
@@ -26,3 +27,10 @@ def set_background(red, green, blue):
         print('Background requires rgb values between 0 and 255')
         return
     background_color = (red, green, blue)
+
+def set_title (title):
+    if not (type(title) is str):
+        print('`title` variable requires a type of `string`')
+        return
+    global screenTitle
+    screenTitle = title
