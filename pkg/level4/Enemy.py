3b3466fe6550560bac284d9b4120333d8b1f393e
Floris de Graaff <florisdegraaff@hotmail.com>
2018-01-12 17:12:59 +0100

    Added player and enemy sprites

--- pkg/level4/Enemy.py
+++ pkg/level4/Enemy.py
@@ -6,18 +6,33 @@ pygame = essentials.pygame
 class Guard:
 
     def __init__ (self):
+        self.image = [
+            pygame.image.load('pkg/level4/images/copLeft2.png'),
+            pygame.image.load('pkg/level4/images/copLeft1.png'),
+            pygame.image.load('pkg/level4/images/copLeft3.png'),
+            pygame.image.load('pkg/level4/images/copLeft1.png')
+        ]
+        self.walkTimer = essentials.timer(0.125)
+        self.currentSprite = 0
         self.position = [1280, 480]
         self.size = (100, 200)
 
     def update (self):
-        self.guard = pygame.draw.rect(display.window, (0,0,0), pygame.Rect((self.position[0], self.position[1]), self.size))
+        if (self.walkTimer.check_timer()):
+            self.walkTimer = essentials.timer (0.125)
+            if self.currentSprite < 3:
+                self.currentSprite+=1
+            else:
+                self.currentSprite = 0
+        self.guard = pygame.draw.rect(display.window, (122,122,122), pygame.Rect((self.position[0], self.position[1]), self.size))
+        display.window.blit(self.image[self.currentSprite], self.position)
         self.position [0] -= 20
 
 class Bullets:
 
     def __init__ (self):
         self.position = [1280, 280]
-        self.size = (20, 20)
+        self.size = (10, 10)
 
     def update (self):
         self.bullet1 = pygame.draw.rect(display.window, (0,0,0), pygame.Rect((self.position[0], self.position[1]), self.size))
--- pkg/level4/Player.py
+++ pkg/level4/Player.py
@@ -12,7 +12,22 @@ class Player:
     jump_counter = -20
 
     def __init__(self):
-        self.rect = pygame.draw.rect(display.window, (0,0,0), pygame.Rect(self.position, self.size))
+        self.isCrawling = False
+        self.rect = pygame.draw.rect(display.window, (122, 122, 122), pygame.Rect(self.position, self.size))
+        self.image = [
+            pygame.image.load('pkg/level4/images/1.walkRight.png'),
+            pygame.image.load('pkg/level4/images/2.walkRight.png'),
+            pygame.image.load('pkg/level4/images/3.walkRight.png'),
+            pygame.image.load('pkg/level4/images/4.walkRight.png')
+        ]
+        self.crawlImages = [
+            pygame.image.load('pkg/level4/images/1.crawl.png'),
+            pygame.image.load('pkg/level4/images/2.crawl.png'),
+            pygame.image.load('pkg/level4/images/3.crawl.png'),
+            pygame.image.load('pkg/level4/images/4.crawl.png')
+        ]
+        self.walkTimer = essentials.timer(0.125)
+        self.currentSprite = 0
 
     def jump (self):
         self.size.y = 200
@@ -25,13 +40,27 @@ class Player:
         return True
 
     def crawl (self):
+        self.isCrawling = True
         self.size.y = 100
         self.position.y = 580
 
+
     def update (self):
-        self.rect = pygame.draw.rect(display.window, (0,0,0), pygame.Rect(self.position, self.size))
+        if (self.walkTimer.check_timer()):
+            self.walkTimer = essentials.timer (0.125)
+            if self.currentSprite < 3:
+                self.currentSprite+=1
+            else:
+                self.currentSprite = 0
+
+        self.rect = pygame.draw.rect(display.window, (122, 122, 122), pygame.Rect(self.position, self.size))
+        if self.isCrawling:
+            display.window.blit(self.crawlImages[self.currentSprite], self.position)
+        else:
+            display.window.blit(self.image[self.currentSprite], self.position)
         self.size.y = 200
         self.position.y = 480
+        self.isCrawling = False
 
     def end_game (self):
         self.position.x = self.position.x + 10
@@ -41,8 +70,8 @@ class Helicopter:
     background_position = Vector2(1280, 0)
 
     def __init__(self):
-        self.background = pygame.image.load("pkg/level4/background.png")
-        self.chopper = pygame.image.load("pkg/level4/choppah.png")
+        self.background = pygame.image.load("pkg/level4/images/background.png")
+        self.chopper = pygame.image.load("pkg/level4/images/choppah.png")
         display.window.blit(self.chopper, self.position)
 
     def update(self):
